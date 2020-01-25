376e16ec75d4be6eebc42582dbc6218fddb69afc
AlonaS <serebrulka90@gmail.com>
2020-01-25 15:49:59 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# First task
+a = 2
+b = 4
+c = 5
+d = 10
+
+print(a + b)
+print(d - a)
+print(a + b + c - d)
+print(d * 50 / 100)
+
+# Second task
+H = 'Hello '
+W = 'World!'
+HW = H + W
+print(HW)
+
+# Third task
+from random import sample
+f = sample(range(0, 100), 10)
+print(f)
+print(f.pop())
+print(f)
+
